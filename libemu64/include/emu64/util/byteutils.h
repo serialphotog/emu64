830d39c4d90a3d3114bc04788f633bf62ecc5921
Adam Thompson <adam@serialphotog.com>
2020-04-10 20:29:24 -0400

    Delete the old memory subsystem

--- libemu64/include/emu64/util/byteutils.h
+++
@@ -1,9 +0,0 @@
-#ifndef __EMU_BYTE_UTILS_H__
-#define __EMU_BYTE_UTILS_H__
-
-#include <stdint.h>
-
-// Performs a byteswap on 32-bit values
-uint32_t bswap_32(uint32_t value);
-
-#endif
--- libemu64/util/byteutils.c
+++
@@ -1,9 +0,0 @@
-#include "emu64/util/byteutils.h"
-
-uint32_t bswap_32(uint32_t value)
-{
-    return ((value >> 24) & 0xff) |
-        ((value << 8) & 0xff0000) |
-        ((value >> 8) & 0xff00) |
-        ((value << 24) & 0xff000000);
-}
